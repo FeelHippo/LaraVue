d4aab122c95446b50fbd6fb2c244f994a0e23687
FeelHippo <mezzasegolas@gmail.com>
2020-07-10 00:16:09 +0200

    app working

--- resources/js/components/ExampleComponent.vue
+++ resources/js/components/ExampleComponent.vue
@@ -36,89 +36,148 @@
                     <button v-show="edit" @click="toggleEdit()" class="btn btn-primary">
                         Cancel
                     </button>
+                    <button class="btn btn-primary" @click="sort">
+                        To original order
+                    </button>
                 </span>
             </div>
         </form>
+
         <h4 class="status1">{{ status[0].title }}</h4>
-        <ul class="column1">
-            <template v-for="task in list">
-                <li class="list-group-item taskCard" v-if="task.status_id === status[0].id" :key="task.body">
-                    <div class="card border-primary mb-1" style="max-width: 20rem;">
-                        <div class="card-header">{{ task.title }}</div>
-                        <div class="card-body">
-                            <h4 class="card-title">{{ task.description }}</h4>
-                            <p class="card-text">{{ task.status }}</p>
+        <draggable
+            class="list-group column1"
+            tag="ul"
+            v-model="list"
+            v-bind="dragOptions"
+            @start="drag = true"
+            @end="drag = false"
+        >
+            <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+                <li 
+                    class="list-group-item taskCard"
+                    v-for="task in list"
+                    v-if="task.status_id === status[0].id"
+                    :key="task.order"
+                >
+                    <div @click="element.fixed = !element.fixed">
+                        <div class="card border-primary mb-1" style="max-width: 20rem;">
+                            <div class="card-header">{{ task.title }}</div>
+                            <div class="card-body">
+                                <h4 class="card-title">{{ task.description }}</h4>
+                                <p class="card-text">{{ task.status }}</p>
+                            </div>
                         </div>
+                        <section class="frontCardButtons">
+                            <button @click="showTask(task.id)" class="btn btn-warning btn-xs pull-right">
+                                Edit
+                            </button>
+                            <button @click="deleteTask(task.id)" class="btn btn-danger btn-xs pull-right">
+                                Delete
+                            </button>
+                        </section>
                     </div>
-                    <section class="frontCardButtons">
-                        <button @click="showTask(task.id)" class="btn btn-warning btn-xs pull-right">
-                            Edit
-                        </button>
-                        <button @click="deleteTask(task.id)" class="btn btn-danger btn-xs pull-right">
-                            Delete
-                        </button>
-                    </section>
                 </li>
-            </template>
-        </ul>
+            </transition-group>
+        </draggable>
+
         <h4 class="status2">{{ status[1].title }}</h4>
-        <ul class="column2">
-            <template v-for="task in list">
-                <li class="list-group-item taskCard" v-if="task.status_id === status[1].id" :key="task.body">
-                    <div class="card border-primary mb-1" style="max-width: 20rem;">
-                        <div class="card-header">{{ task.title }}</div>
-                        <div class="card-body">
-                            <h4 class="card-title">{{ task.description }}</h4>
-                            <p class="card-text">{{ task.status }}</p>
+        <draggable
+            class="list-group column2"
+            tag="ul"
+            v-model="list"
+            v-bind="dragOptions"
+            @start="drag = true"
+            @end="drag = false"
+        >
+            <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+                <li 
+                    class="list-group-item taskCard"
+                    v-for="task in list"
+                    v-if="task.status_id === status[1].id"
+                    :key="task.order"
+                >
+                    <div @click="element.fixed = !element.fixed">
+                        <div class="card border-primary mb-1" style="max-width: 20rem;">
+                            <div class="card-header">{{ task.title }}</div>
+                            <div class="card-body">
+                                <h4 class="card-title">{{ task.description }}</h4>
+                                <p class="card-text">{{ task.status }}</p>
+                            </div>
                         </div>
+                        <section class="frontCardButtons">
+                            <button @click="showTask(task.id)" class="btn btn-warning btn-xs pull-right">
+                                Edit
+                            </button>
+                            <button @click="deleteTask(task.id)" class="btn btn-danger btn-xs pull-right">
+                                Delete
+                            </button>
+                        </section>
                     </div>
-                    <section class="frontCardButtons">
-                        <button @click="showTask(task.id)" class="btn btn-warning btn-xs pull-right">
-                            Edit
-                        </button>
-                        <button @click="deleteTask(task.id)" class="btn btn-danger btn-xs pull-right">
-                            Delete
-                        </button>
-                    </section>
                 </li>
-            </template>
-        </ul>
+            </transition-group>
+        </draggable>
+
         <h4 class="status3">{{ status[2].title }}</h4>
-        <ul class="column3">
-            <template v-for="task in list">
-                <li class="list-group-item taskCard" v-if="task.status_id === status[2].id" :key="task.body">
-                    <div class="card border-primary mb-1" style="max-width: 20rem;">
-                        <div class="card-header">{{ task.title }}</div>
-                        <div class="card-body">
-                            <h4 class="card-title">{{ task.description }}</h4>
-                            <p class="card-text">{{ task.status }}</p>
+        <draggable
+            class="list-group column3"
+            tag="ul"
+            v-model="list"
+            v-bind="dragOptions"
+            @start="drag = true"
+            @end="drag = false"
+        >
+            <transition-group type="transition" :name="!drag ? 'flip-list' : null">
+                <li 
+                    class="list-group-item taskCard"
+                    v-for="task in list"
+                    v-if="task.status_id === status[2].id"
+                    :key="task.order"
+                >
+                    <div @click="element.fixed = !element.fixed">
+                        <div class="card border-primary mb-1" style="max-width: 20rem;">
+                            <div class="card-header">{{ task.title }}</div>
+                            <div class="card-body">
+                                <h4 class="card-title">{{ task.description }}</h4>
+                                <p class="card-text">{{ task.status }}</p>
+                            </div>
                         </div>
+                        <section class="frontCardButtons">
+                            <button @click="showTask(task.id)" class="btn btn-warning btn-xs pull-right">
+                                Edit
+                            </button>
+                            <button @click="deleteTask(task.id)" class="btn btn-danger btn-xs pull-right">
+                                Delete
+                            </button>
+                        </section>
                     </div>
-                    <section class="frontCardButtons">
-                        <button @click="showTask(task.id)" class="btn btn-warning btn-xs pull-right">
-                            Edit
-                        </button>
-                        <button @click="deleteTask(task.id)" class="btn btn-danger btn-xs pull-right">
-                            Delete
-                        </button>
-                    </section>
                 </li>
-            </template>
-        </ul>
+            </transition-group>
+        </draggable>
     </div>
 </template>
 
 <script>
+
+    import draggable from 'vuedraggable'
+
     export default {
+        name: "ToDoList",
+        display: "Transitions",
+        order: 7,
+        components: {
+            draggable
+        },
         data: function() {
             return {
                 edit: false,
+                drag: false,
                 list: [],
                 task: {
                     id: '',
                     title: '',
                     description: '',
                     status_id: 1,
+                    order: 0
                 },
                 status: []
             };
@@ -130,7 +189,12 @@
         methods: {
             fetchTaskList: function() {
                 this.$http.get('api/tasks').then(response => {
-                    this.list = response.data
+                    this.list = response.data.map((task, index) => {
+                        return {
+                            ...task,
+                            order: index + 1
+                        };
+                    })
                 });
             },
             fetchStatusList: function() {
@@ -143,6 +207,9 @@
             },
             toggleEdit() {
                 this.edit = !this.edit
+            },
+            sort() {
+                this.list = this.list.sort((a, b) => a.order - b.order)
             },
             createTask: function() {
                 this.$http.post('api/tasks/store', this.task)
@@ -177,6 +244,16 @@
                 this.$http.delete('api/tasks/' + id).then(() => {
                     this.fetchTaskList()
                 })
+            }
+        },
+        computed: {
+            dragOptions() {
+                return {
+                    animation: 200,
+                    group: "description",
+                    disabled: false,
+                    ghostClass: "ghost"
+                };
             }
         }
     }    
